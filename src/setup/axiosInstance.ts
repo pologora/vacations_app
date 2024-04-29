8b2a8945a1e289e4e2209364b1e2e55568a5a6e3
Oleksandr <lysakova@yahoo.com>
2024-04-29 08:40:31 +0200

    feat: change base url for demo heroku url

--- src/setup/axiosInstance.ts
+++ src/setup/axiosInstance.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://www.server.snti.pl/api/v1';
-// const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = 'https://magazyn-demo-30f1f8a297d3.herokuapp.com/api/v1';
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
